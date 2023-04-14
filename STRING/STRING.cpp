32a90eede7ecf2d23252fdae5cc203dad54bf2f6
777 <777@DESKTOP-T0PRUM1>
2023-04-14 19:01:23 +0800

    ConstructorsDelegetion

--- STRING/STRING.cpp
+++ STRING/STRING.cpp
@@ -17,19 +17,19 @@ public:
         //this->str = new char[size] {};
         cout << "Default1ArgConstructor:\t" << this << endl;
     }
-    String(const char* str):size(strlen(str) + 1), str(new char[size] {})
+    String(const char* str):String(strlen(str)+1)
     {
         /*this->size = strlen(str)+1;
         this->str = new char[size] {};*/
         for (int i = 0; i < size; i++)this->str[i] = str[i];
         cout << "Default1ArgConstructor1:\t" << this << endl;
     }
-    String(const String& other):size(other.size),str(new char [size] {})
+    String(const String& other):String(other.str)
     {
         //deep copy
         /*this->size = other.size;
         this->str = new char [size]{};*/
-        for (int i = 0; i < this->size; i++) this->str[i] = other.str[i];
+        //for (int i = 0; i < this->size; i++) this->str[i] = other.str[i];
         cout << "CopyConstructor:\t" << this << endl;
     }
     String(String&& other)
